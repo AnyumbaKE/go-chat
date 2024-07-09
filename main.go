a4f50b584c8eefcb8342b19b01ec0aa80fb1c9c9
AnyumbaKE <esanyumba@gmail.com>
2024-07-09 22:15:19 +0300

    Configuring Static files

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
 
 func main() {
+	// Create views engine
+	viewsEngine := html.New("./views", ".html")
+
 	// Start new fiber instance
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Views: viewsEngine,
+	})
+
+	// Static route and directory
+	app.Static("/static/", "./static")
 
 	// Create a "ping" handler to test the server
 	app.Get("/ping", func(ctx *fiber.Ctx) error {
